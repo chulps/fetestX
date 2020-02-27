a53765253c14e3222bd8400c12ae756c56033434
Ben Junya <bjunya@gmail.com>
2020-02-27 10:17:23 -0800

    CSS and indentation cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,16 @@ import PasswordField from './PasswordField';
 const App: React.FC<{}> = () => {
   const theme = useTheme();
   const style = css`
-    .wrapper {
-      height: 100vh;
-      width: 100vw;
-      background-color: bisque;
-    }
-
     .inner {
       max-width: 500px;
       margin: 0 auto;
+      margin-top: ${theme.spacing(10)}px;
+    }
+
+    .controller {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
     }
 
     .submit-button {
@@ -39,34 +40,34 @@ const App: React.FC<{}> = () => {
   return (
     <div className="container" css={style}>
       <div className="inner">
-      <Typography variant="body1">
-        Let&apos;s make a Form Library!
-      </Typography>
-
-      <Typography variant="body2">
-        Things to do:<br/><br/>
-
-        Make the password field show/hide its value<br/><br/>
-        Store the value of the form in an object taking the form of {`{ name: value }`}<br/><br/>
-        Keep this value in the state of {`<FormController>`}<br/><br/>
-        Submitting the form should run "onSubmit()"<br/><br/>
-
-
-
-
-      </Typography>
-      <FormController onSubmit={onSubmit}>
-        <InputField name="email" label="Email address?" /><br/>
-        <InputField name="full_name" label="Your full name here" /><br/>
-        <PasswordField name="password" label="Password" /><br/>
-        <Button className="submit-button" type="submit" variant="outlined">Submit this form!</Button>
-      </FormController>
-      {submitValues && (
         <Typography variant="body1">
-          You submitted<br/><br/>
-          {submitValues}
+          Let&apos;s make a Form Library!
         </Typography>
-      )}
+
+        <Typography variant="body2">
+          Things to do:<br/><br/>
+
+          Make the password field show/hide its value<br/><br/>
+          Store the value of the form in an object taking the form of {`{ name: value }`}<br/><br/>
+          Keep this value in the state of {`<FormController>`}<br/><br/>
+          Submitting the form should run "onSubmit()"<br/><br/>
+
+
+
+
+        </Typography>
+        <FormController className="controller" onSubmit={onSubmit}>
+          <InputField name="email" label="Email address?" />
+          <InputField name="full_name" label="Your full name here" />
+          <PasswordField name="password" label="Password" />
+          <Button className="submit-button" type="submit" variant="outlined">Submit this form!</Button>
+        </FormController>
+        {submitValues && (
+          <Typography variant="body1">
+            You submitted<br/><br/>
+            {submitValues}
+          </Typography>
+        )}
       </div>
     </div>
   );
